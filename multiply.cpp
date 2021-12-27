9647ebde35f1867aecfcb31a2f8103f9099776db
SushmithKarun <43995516+SushmithKarun@users.noreply.github.com>
2021-12-27 20:43:57 +0530

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+  double n1,n2,p;
+  cout<<"Enter two numbers: ";
+  cin>>n1>> n2;
+  product=n1*n2;  
+  cout<<"Product = "<<p;        
+}
